43c257c51bf1eabdc453f53b56968758e3d2c36a
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-31 15:47:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   
   def create 
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to
+    redirect_to students_path 
   end 
 
   def show
